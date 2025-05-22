a1b8445d301c8ca0b2d1d32cbdf4dc192dbe8284
daviddbyrd <davbywork@gmail.com>
2025-05-22 17:30:51 +0100

    Add styling to NavBar.

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,9 +1,9 @@
 const NavBar: React.FC = () => {
   return (
-    <div className="fixed w-full h-12 backdrop-blur flex flex-row justify-left top-0">
-      <div className="ml-10 cursor-pointer">Flow MES</div>
-      <div className="ml-10 cursor-pointer">Execute</div>
-      <div className="ml-10 cursor-pointer">Create</div>
+    <div className="fixed w-full h-12 backdrop-blur flex flex-row justify-left items-center top-0 border-b border-gray-100">
+      <div className="ml-10 cursor-pointer text-lg font-bold">Flow MES</div>
+      <div className="ml-32 cursor-pointer text-lg text-gray-600">Execute</div>
+      <div className="ml-32 cursor-pointer text-lg text-gray-600">Create</div>
     </div>
   );
 };
