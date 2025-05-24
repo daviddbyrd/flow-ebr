6e1d4309b850e503c7a0be1b3c7dcb4984074433
daviddbyrd <davbywork@gmail.com>
2025-05-24 12:26:16 +0100

    Added navigation to navbar.

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,9 +1,29 @@
+import { useNavigate } from "react-router-dom";
+
 const NavBar: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = (e: React.MouseEvent<HTMLButtonElement>) => {
+    navigate(`/${e.currentTarget.name}`, { replace: true });
+  };
+
   return (
     <div className="fixed w-full h-12 backdrop-blur flex flex-row justify-left items-center top-0 border-b border-gray-100">
       <div className="ml-10 cursor-pointer text-lg font-bold">Flow MES</div>
-      <div className="ml-32 cursor-pointer text-lg text-gray-600">Execute</div>
-      <div className="ml-32 cursor-pointer text-lg text-gray-600">Create</div>
+      <button
+        name="execute"
+        onClick={(e) => handleNavigate(e)}
+        className="ml-32 cursor-pointer text-lg text-gray-600"
+      >
+        Execute
+      </button>
+      <button
+        name="create"
+        onClick={(e) => handleNavigate(e)}
+        className="ml-32 cursor-pointer text-lg text-gray-600"
+      >
+        Create
+      </button>
     </div>
   );
 };
